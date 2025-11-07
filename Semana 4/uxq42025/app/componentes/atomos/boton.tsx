824b5ef59728bcc7a1d5bdc85499eaf87b4f1b15
Claudia Cortes <claudiacortes@MacBook-Pro-de-Claudia.local>
2025-11-06 18:26:24 -0600

    clase 6 de noviembre: parametros en componentes funcionales

--- Semana 4/uxq42025/app/componentes/atomos/boton.tsx
+++ Semana 4/uxq42025/app/componentes/atomos/boton.tsx
@@ -1,7 +1,16 @@
-export default function Boton(){
+interface BotonParams {
+    titulo: string;
+    customStyle: string;
+}
+// parametros.titulo
+// {titulo, customStyle} = parametros
+export default function Boton({ titulo, customStyle }: BotonParams) {
+
     return (
-        <button>
-            HOLA MUNDO
+        <button
+            className={`flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px] ${customStyle}`}
+        >
+            {titulo}
         </button>
     )
 }
